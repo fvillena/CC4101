13c42f03b959bfba779e6c213edbe887871a515b
Fabián Villena <villenafabian@gmail.com>
2022-09-25 19:25:50 -0300

    exercise 2

--- T2/T2.rkt
+++ T2/T2.rkt
@@ -86,31 +86,122 @@ s-Cmd ::=
             EJERCICIO 2
    ============================== |#
 
+;; *Código extraído de la clase 7*
+;; Interface of the Abstract Dada Type (ADT) for  
+;; keeping track of the deferred substitutions
+
+;; empty-env  :: Env
+;; extend-env :: Symbol Value Env -> Env
+;; env-lookup :: Symbol Env -> Value
+
+;; Implementation of the ADT
+
+;; <env> ::= mtEnv
+;;         | (aEnv <id> <value> <env>)
+
+(deftype Env
+  (mtEnv)
+  (aEnv id val env))
+
+(define empty-env (mtEnv))
+
+(define extend-env aEnv)
+
+(define (env-lookup x env)
+  (match env
+    [(mtEnv) (error 'env-lookup "free identifier: ~a" x)]
+    [(aEnv id val rest) (if (symbol=? id x)
+                            val
+                            (env-lookup x rest))]))
+
 #| PARTE A |#
+
 ;; check-table :: Cmd -> Boolean / Error
+;; comprueba que las tablas donde se insertan o consultan datos
+;; se encuentren previamente definidas
+(define (check-table Cmd)
+  (check-table-with-env Cmd empty-env))
 
+;; check-table-with-env :: Cmd Env -> Boolean / Error
+;; función auxiliar que complementa check-table para
+;; el uso de ambientes
+(define (check-table-with-env Cmd env)
+  (match Cmd
+    [(CREATE table-name column-names cmd) (check-table-with-env cmd (extend-env table-name 'dummy env))]
+    [(INSERT row table-name cmd) (if (exists-in-env? table-name env) #t (error (format "Error Estatico A1: Registro ~a insertado en tabla indefinida ~a." row table-name))) (check-table-with-env cmd env) ]
+    [(FROM table-name 'SELECT 'regs 'WHERE cond) (if (exists-in-env? table-name env) #t (error (format "Error Estatico A2: La tabla consultada ~a no se encuentra definida." table-name)))]
+    ))
 
+;; exists-in-env :: Symbol Env -> Boolean
+;; función auxiliar que verifica si existe una
+;; tabla en un ambiente
+(define (exists-in-env? x env)
+  (with-handlers ([exn:fail? (lambda (exn) #f)]) (env-lookup x env) #t))
 
 
 #| PARTE B |#
+
 ;; check-arity :: Cmd -> Boolean / Error
+;; comprueba que al insertar registros la aridad sea consistente
+(define (check-arity Cmd)
+  (check-arity-with-env Cmd empty-env))
 
+;; check-arity-with-env :: Cmd Env -> Boolean / Error
+;; función auxiliar que complementa check-arity para
+;; el uso de ambientes
+(define (check-arity-with-env Cmd env)
+  (match Cmd
+    [(CREATE table-name column-names cmd) (check-arity-with-env cmd (extend-env table-name column-names env))]
+    [(INSERT row table-name cmd) (if (arity-match? row table-name env) #t (error (format "Error Estatico B: Registro ~a con aridad ~a insertado en tabla ~a de aridad ~a." row (length row) table-name (length (env-lookup table-name env))))) (check-arity-with-env cmd env) ]
+    [(FROM table-name 'SELECT 'regs 'WHERE cond) #t]
+    ))
 
+;; arity-match? :: String Symbol Env -> Boolean
+;; función auxiliar que verifica la consistencia
+;; de en la aridad de una nueva fila y la aridad de la tabla
+(define (arity-match? row table-name env)
+  (equal? (length (env-lookup table-name env)) (length row)))
 
 #| PARTE C |#
+
 ;; check-column :: Cmd -> Boolean / Error
+;; verifica que los nombres consultados sean consistentes
+(define (check-column Cmd)
+  (check-column-with-env Cmd empty-env))
 
+;; check-column-with-env :: Cmd Env -> Boolean / Error
+;; función auxiliar que complementa check-column para
+;; el uso de ambientes
+(define (check-column-with-env Cmd env)
+  (match Cmd
+    [(CREATE table-name column-names cmd) (check-column-with-env cmd (extend-env table-name column-names env))]
+    [(INSERT row table-name cmd) (check-column-with-env cmd env) ]
+    [(FROM table-name 'SELECT 'regs 'WHERE cond) (check-cond-columns cond table-name env)]
+    ))
 
+;; check-cond-columns :: Cond Symbol Env -> Boolean / Error
+;; verifica que las columnas consultadas en WHERE
+;; estén presentes en la tabla
+(define (check-cond-columns Cond table-name env)
+  (match Cond
+    [(or (< s n) (> s n) (= s n)) (if (column-in-table? s table-name env) #t (error (format "Error Estático C: La columna ~a no está definida en la tabla ~a." s table-name)))]
+    [(or (orb lc rc) (& lc rc)) (check-cond-columns lc table-name env) (check-cond-columns rc table-name env)]))
+
+;; column-in-table? :: String Symbol Env -> Boolean
+;; función auxiliar que verifica si una columna está
+;; en la tabla
+(define (column-in-table? column table-name env)
+  (if (member column (env-lookup table-name env)) #t #f))
 
 #| PARTE D |#
+
 ;; static-check :: Cmd -> Boolean / Error
-
-
-
-#| PARTE F |#
-;; end-time :: TaskSchedule string -> integer
-
-
+;; realiza los checking estáticos
+;; check-table, check-arity y check-column
+(define (static-check Cmd)
+  (check-table Cmd)
+  (check-arity Cmd)
+  (check-column Cmd))
 
 #| ==============================
             EJERCICIO 3
