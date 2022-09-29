e4dc4a0aac5b2e9144b77104e1d2982513f6349b
Fabián Villena <villenafabian@gmail.com>
2022-09-28 23:05:54 -0300

    full tests

--- T2/test.rkt
+++ T2/test.rkt
@@ -124,6 +124,9 @@
 (test/exn (check-table (parse my-s-Cmd-ill-query)) "Error Estatico A2: La tabla consultada B no se encuentra definida")
 (test/exn (check-table (parse my-s-Cmd-ill-query2)) "Error Estatico A2: La tabla consultada C no se encuentra definida")
 
+(test (exists-in-env? 'A (aEnv 'A 'dummy 'dummy (mtEnv))) #t)
+(test (exists-in-env? 'B (aEnv 'A 'dummy 'dummy (mtEnv))) #f)
+
 #| PARTE B |#
 
 (test (check-arity (parse my-s-Cmd)) #t)
@@ -132,6 +135,9 @@
 (test (check-arity (parse my-s-Cmd-nested-create2)) #t)
 (test/exn (check-arity (parse my-s-Cmd-ill-arity)) "Error Estatico B: Registro (1 2 3 4) con aridad 4 insertado en tabla A de aridad 3")
 (test/exn (check-arity (parse my-s-Cmd-ill-arity2)) "Error Estatico B: Registro (7 8 9 10 11) con aridad 5 insertado en tabla B de aridad 4")
+
+(test (arity-match? '(1 2 3) 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) #t)
+(test (arity-match? '(1 2 3 4) 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) #f)
 
 #| PARTE C |#
 
@@ -144,9 +150,13 @@
 (test/exn (check-column (parse my-s-Cmd-ill-column3)) "Error Estático C: La columna a no está definida en la tabla B")
 (test/exn (check-column (parse my-s-Cmd-ill-column4)) "Error Estático C: La columna d no está definida en la tabla A")
 
+(test (check-cond-columns (= 'a 1) 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) #t)
+(test/exn (check-cond-columns (= 'd 1) 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) "Error Estático C: La columna d no está definida en la tabla A")
+
+(test (column-in-table? 'a 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) #t)
+(test (column-in-table? 'd 'A (aEnv 'dummy 'A '(a b c) (mtEnv))) #f) 
+
 #| PARTE D |#
-
-
 
 (test (static-check (parse my-s-Cmd)) #t)
 (test (static-check (parse my-s-Cmd2)) #t)
@@ -175,6 +185,16 @@
 (test (interp-cmd (parse my-s-Cmd2) empty-env) '((1 2 3)))
 (test (interp-cmd (parse my-s-Cmd-nested-create2) empty-env) '((7 8 9)))
 (test (interp-cmd (parse my-s-Cmd-nested-create3) empty-env) '((1 2 3)))
+
+(test (interp-cond 'A (= 'a 1) (aEnv 'A 'reg '(1 2 3) (aEnv 'A 'column-names '(a b c) (mtEnv)))) '((1 2 3)))
+(test (interp-cond 'A (= 'a 1) (aEnv 'A 'reg '(1 4 4) (aEnv 'A 'reg '(1 2 3) (aEnv 'A 'column-names '(a b c) (mtEnv))))) '((1 2 3) (1 4 4)))
+
+(test (all-but-last '(1 2 3)) '(1 2))
+(test (all-but-last '((1 2 3) (4 5 6))) '((1 2 3)))
+
+(test (index-of '(1 2 3) 1) 0)
+
+(test (env-lookup2 'A 'reg (aEnv 'A 'reg '(1 4 4) (aEnv 'A 'reg '(1 2 3) (aEnv 'A 'column-names '(a b c) (mtEnv))))) '((1 4 4) (1 2 3) (dummy)))
 
 #| PARTE B |#
 
